6a54cc17486fdac4dd53dc495dace7e6575cbce3
x <>
2024-06-17 12:30:16 +0800

    Update useAllPairlist hook

--- src/hooks/useAllPairList.ts
+++ src/hooks/useAllPairList.ts
@@ -3,6 +3,7 @@ import { useConnex } from "@vechain/dapp-kit-react";
 import { find } from "lodash";
 import IUniswapV2Factory from "~/abis/IUniswapV2Factory.json";
 import IUniswapV2Pair from "~/abis/IUniswapV2Pair.json";
+import API_ERC20 from "~/abis/erc20.json";
 import { FACTORY_ADDRESS } from "~/constants/addresses";
 
 export default function useAllPairList() {
@@ -10,7 +11,7 @@ export default function useAllPairList() {
 
   return useQuery({
     queryKey: ["all-pair-list"],
-    refetchInterval: 1000 * 15,
+    refetchInterval: 1000 * 180,
     queryFn: () => {
       return connex.thor
         .account(FACTORY_ADDRESS)
@@ -33,13 +34,36 @@ export default function useAllPairList() {
           const pairAddressList = data.map((i: any) => i.decoded["0"]);
           return Promise.all(
             pairAddressList.map((i: any) =>
-              connex.thor
-                .account(i)
-                .method(find(IUniswapV2Pair.abi, { name: "getReserves" }))
-                .call()
+              Promise.all([
+                connex.thor
+                  .account(i)
+                  .method(find(IUniswapV2Pair.abi, { name: "getReserves" }))
+                  .call(),
+                connex.thor
+                  .account(i)
+                  .method(find(API_ERC20, { name: "totalSupply" }))
+                  .call(),
+                connex.thor
+                  .account(i)
+                  .method(find(IUniswapV2Pair.abi, { name: "token0" }))
+                  .call(),
+                connex.thor
+                  .account(i)
+                  .method(find(IUniswapV2Pair.abi, { name: "token1" }))
+                  .call()
+              ])
             )
           );
         });
+    },
+    select: (data: any) => {
+      return data.map((i: any) => ({
+        reserve0: i[0].decoded["reserve0"],
+        reserve1: i[0].decoded["reserve1"],
+        lpTotalSupply: i[1].decoded["0"],
+        token0Address: i[2].decoded["0"],
+        token1Address: i[3].decoded["0"]
+      }));
     }
   });
 }
