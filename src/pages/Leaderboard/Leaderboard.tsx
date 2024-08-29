ca297a746fa057d7d833773f33d00cc8c6432790
pwv2 <pwv2@outlook.com>
2024-08-29 22:33:26 +0800

    Add weekly points

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,6 +1,7 @@
+import { Tabs, TabList, Tab, TabPanel } from "react-aria-components";
 import Table from "~/components/Table";
+import { totalPoints, weeklyPoints } from "~/data/points";
 import css from "./Leaderboard.module.scss";
-import { accumulatedData } from "~/data/total-points";
 
 export default function Leaderboard() {
   return (
@@ -16,27 +17,66 @@ export default function Leaderboard() {
         </div>
       </section>
       {/* TODO: Show top 10 & Show All */}
+
       <section className={css.section}>
         <h2 className={css.section__heading}>Top Contributors</h2>
-        <p className={css.section__subheading}>* Points are updated every week</p>
-        <Table>
-          <thead>
-            <tr>
-              <th>Rank</th>
-              <th>Address</th>
-              <th className={css.point}>Points Earned</th>
-            </tr>
-          </thead>
-          {accumulatedData.map((item, index) => (
-            <tbody key={item[0]}>
-              <tr key="-">
-                <td className={css.mono}>{index + 1}</td>
-                <td className={css.mono}>{item[0]}</td>
-                <td className={css.point}>{item[1]}</td>
-              </tr>
-            </tbody>
-          ))}
-        </Table>
+        <Tabs>
+          <TabList className={css.tabList} aria-label="Transactions Tabs">
+            <Tab className={css.tabButton} id="total">
+              Cumulative Points
+            </Tab>
+            <Tab className={css.tabButton} id="weekly">
+              Points This Week
+            </Tab>
+          </TabList>
+          <TabPanel id="total">
+            <p className={css.section__subheading}>* Points are updated every week</p>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Rank</th>
+                  <th>Address</th>
+                  <th className={css.point}>Points Earned</th>
+                </tr>
+              </thead>
+              {totalPoints.map((item, index) => (
+                <tbody key={item[0]}>
+                  <tr key="-">
+                    <td className={css.mono}>{index + 1}</td>
+                    <td className={css.mono}>{item[0]}</td>
+                    <td className={css.point}>{item[1]}</td>
+                  </tr>
+                </tbody>
+              ))}
+            </Table>
+          </TabPanel>
+          <TabPanel id="weekly">
+            <p className={css.section__subheading}>* Data from August 19 through August 25, 2024</p>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Rank</th>
+                  <th>Address</th>
+                  <th className={css.point}>Points Earned</th>
+                </tr>
+              </thead>
+              {weeklyPoints.map((item, index) => (
+                <tbody key={item[0]}>
+                  <tr key="-">
+                    <td className={css.mono}>{index + 1}</td>
+                    <td className={css.mono}>
+                      {item[0]}
+                      {!!item[2] && <span className={css.doubleIndicator}>VOTED</span>}
+                    </td>
+                    <td className={css.point}>
+                      {!!item[2] ? <span className={css.doubleIndicator}>{+item[1] / 2}x2</span> : item[1]}
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            </Table>
+          </TabPanel>
+        </Tabs>
       </section>
     </div>
   );
