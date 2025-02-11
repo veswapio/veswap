e959005d3ba62adb5d43ef9f1fddf72341086e62
pwv2 <pwv2@outlook.com>
2025-02-11 21:05:16 +0800

    Update icons

--- src/constants/tokenIcons.tsx
+++ src/constants/tokenIcons.tsx
@@ -1,11 +1,11 @@
-import IconVET from "~/assets/tokens/vet.svg?react";
-import IconVTHO from "~/assets/tokens/vtho.svg?react";
+import IconVET from "~/assets/tokens/VET_Token_Icon.png";
+import IconVTHO from "~/assets/tokens/VTHO_Token_Icon.png";
 import IconB3TR from "~/assets/tokens/b3tr.svg?react";
 
 const TOKEN_ICONS: { [key: string]: any } = {
-  VET: <IconVET />,
-  VVET: <IconVET />,
-  VTHO: <IconVTHO />,
+  VET: <img width="20" src={IconVET} alt="" />,
+  VVET: <img width="20" src={IconVET} alt="" />,
+  VTHO: <img width="20" src={IconVTHO} alt="" />,
   B3TR: <IconB3TR />
 };
 
