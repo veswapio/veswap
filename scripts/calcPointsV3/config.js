c426a195205929bd41058ca596161f05fc18f121
pwv2 <pwv2@outlook.com>
2025-02-24 12:45:28 +0800

    Add round 34 data

--- scripts/calcPointsV3/config.js
+++ scripts/calcPointsV3/config.js
@@ -1,3 +1,3 @@
-export const END_TIME = "2025-02-16 23:59:59.999Z";
+export const END_TIME = "2025-02-23 23:59:59.999Z";
 export const ENABLE_DEBUG = false;
 export const DEBUG_ADDRESS = "0x557f28e912ba393aeb92cb0c1fd178fdd3e70e13".toLowerCase();
